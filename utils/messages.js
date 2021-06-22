8dfc672f64dabfc492f39dd808d4775996f4150e
rjrajshinde <rjrajshinde@gmail.com>
2021-06-22 14:31:31 +0530

    datechanged

--- utils/messages.js
+++ utils/messages.js
@@ -1,11 +1,11 @@
-const moment = require('moment');
+const moment = require('moment-timezone');
 
 function formatMessage(username, text) {
     return {
         username, 
         text,
-        time: moment().format('h:mm a') //using moment.js we format this time
-    }
+        time: moment().tz("Asia/Kolkata").format('h:mm a') //using moment.js we format this time
+    };
 }
 
 module.exports = formatMessage;
